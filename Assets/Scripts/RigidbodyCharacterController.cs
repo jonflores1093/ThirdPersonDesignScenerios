a302a629ad930d4770689d6a5b70dd3b5976a55b
Jon Flores <61673020+jonflores1093@users.noreply.github.com>
2020-11-24 15:25:17 -0600

    Added Frictions on objects and player

--- Assets/Scripts/RigidbodyCharacterController.cs
+++ Assets/Scripts/RigidbodyCharacterController.cs
@@ -10,26 +10,46 @@ public class RigidbodyCharacterController : MonoBehaviour
     [SerializeField]
     private float maxSpeed = 2;
 
+
+    [SerializeField]
+    private PhysicMaterial stoppingPhysicsMaterial, movingPhysicsMaterial;
+
+
     private new Rigidbody rigidbody;
     private Vector2 input;
-
+    private new Collider collider;
 
 
 
     private void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
-
+        collider = GetComponent<Collider>();
     }
 
     private void FixedUpdate()
     {
         var inputDirection = new Vector3(input.x, 0, input.x);
 
+        collider.material = inputDirection.magnitude > 0 ? movingPhysicsMaterial : stoppingPhysicsMaterial;
+
+        //if (inputDirection.magnitude > 0)
+        //{
+
+        //    collider.material = movingPhysicsMaterial;
+        //}
+
+        //else
+        //{
+        //    collider.material = stoppingPhysicsMaterial;
+
+        //}
+
+
         if (rigidbody.velocity.magnitude < maxSpeed)
         {
 
-            rigidbody.AddForce(inputDirection * accerlationForce);
+            rigidbody.AddForce(inputDirection * accerlationForce, ForceMode.Acceleration);
 
         }
         
