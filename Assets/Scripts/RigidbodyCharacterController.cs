482dd82919dcebeb43ed636c1cef3d141e95f03b
Jon Flores <61673020+jonflores1093@users.noreply.github.com>
2020-11-24 14:31:03 -0600

    Add movement and maxspeed

---
+++ Assets/Scripts/RigidbodyCharacterController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidbodyCharacterController : MonoBehaviour
+{
+    [SerializeField]
+    private float accerlationForce = 10;
+
+    [SerializeField]
+    private float maxSpeed = 2;
+
+    private new Rigidbody rigidbody;
+    private Vector2 input;
+
+
+
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+
+    }
+
+    private void FixedUpdate()
+    {
+        var inputDirection = new Vector3(input.x, 0, input.x);
+
+        if (rigidbody.velocity.magnitude < maxSpeed)
+        {
+
+            rigidbody.AddForce(inputDirection * accerlationForce);
+
+        }
+        
+    }
+
+
+    private void Update()
+    {
+
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+
+    }
+}
