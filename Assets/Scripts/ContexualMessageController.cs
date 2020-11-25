f10f7a3e1b1e60a5710074b17a9a056b735eb9df
Jon Flores <61673020+jonflores1093@users.noreply.github.com>
2020-11-24 20:46:55 -0600

    Created new script for contexual message

---
+++ Assets/Scripts/ContexualMessageController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ContexualMessageController : MonoBehaviour
+{
+    private CanvasGroup canvasGroup;
+    private TMP_Text messageText;
+
+    private void Start()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+        messageText = GetComponent<TMP_Text>();
+
+        canvasGroup.alpha = 0;
+    }
+
+    private void ShowMessage(string message, float duration)
+    {
+        messageText.text = message;
+
+
+
+    }
+
+
+
+
+}
