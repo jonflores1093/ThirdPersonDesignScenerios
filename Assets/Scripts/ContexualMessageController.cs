c2e229516785138593a2c89ae6018ccf4c07faf2
Jon Flores <61673020+jonflores1093@users.noreply.github.com>
2020-11-24 20:53:07 -0600

    Added a coroutine

--- Assets/Scripts/ContexualMessageController.cs
+++ Assets/Scripts/ContexualMessageController.cs
@@ -14,12 +14,17 @@ public class ContexualMessageController : MonoBehaviour
         messageText = GetComponent<TMP_Text>();
 
         canvasGroup.alpha = 0;
+
+        StartCoroutine(ShowMessage("Testing", 2));
+
     }
 
-    private void ShowMessage(string message, float duration)
+    private IEnumerator ShowMessage(string message, float duration)
     {
+        canvasGroup.alpha = 1;
         messageText.text = message;
-
+        yield return new WaitForSeconds(duration);
+        canvasGroup.alpha = 0;
 
 
     }
