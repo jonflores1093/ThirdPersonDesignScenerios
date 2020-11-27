aa4392636d8a502e5b2b886d5430c04d6ebb113e
Jon Flores <61673020+jonflores1093@users.noreply.github.com>
2020-11-26 21:36:45 -0600

    Used Singleton scripts in new scene

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoSingleton<PlayerScript>
+{
+   
+
+    public string name;
+
+
+    public override void Init()
+    {
+        base.Init();
+
+        Debug.Log("Player Initialized");
+    }
+
+
+    //void Start()
+    //{
+
+
+    //    GameManager.Instance.DisplayName();
+    //    UIManager.Instance.UpdateScore(40);
+    //    SpawnManager.Instance.StartSpawning();
+
+    //}
+
+
+    //void Update()
+    //{
+
+    //}
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    private static SpawnManager _instance;
+    public static SpawnManager Instance
+    {
+
+        get
+        {
+
+            if (_instance == null)
+                Debug.LogError("Spawn Manager is NULL");
+            return _instance;
+        }
+
+    }
+
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+    public void StartSpawning()
+    {
+
+        Debug.Log("Spawn Started");
+
+
+    }
+
+    private void Start()
+    {
+
+        Debug.Log(PlayerScript.Instance.name);
+
+    }
+
+}
